1bdec0b1b7220410035a2c36dd0aa076146ac04a
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-08-18 02:15:17 +0000

    Initial commit

---
+++ public/static/game.js
@@ -0,0 +1,7 @@
+const againstSystem = document.getElementById("against-the-system");
+
+const menu1 = document.getElementById("menu1");
+
+againstSystem.onclick = function () {
+  menu1.style.display = "none";
+}
